40138d95cc34baa88a3ebf9b5c475bb6a6ac04c8
halitkotlin <94926546+halitkotlin@users.noreply.github.com>
2023-07-23 16:04:23 +0300

    Logout Button
    Logout Button Eklendi

--- app/src/main/java/com/example/loginautologouttest/MainActivity.java
+++ app/src/main/java/com/example/loginautologouttest/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.loginautologouttest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    String uye_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("giris", 0);
+
+        button = findViewById(R.id.logoutButon);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
